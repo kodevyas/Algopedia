2346818bfa46917628d050b11a3ec68896c00481
rahul1809 <rahul.mishra2003@gmail.com>
2016-08-21 22:34:26 +0530

    sorting algorithms added

---
+++ Algorithms Edition1/Sorting/BubbleSort.cpp
@@ -0,0 +1,44 @@
+/*
+Bubble sorting algorithm
+O(n^2)
+non stable, comparison  based algorithm.
+O(1) of auxiliary space required.
+Introduced variable swapped if no swapping occurs during single pass we should 
+break then and there only, that's the only optimisation possible.
+
+
+The only advantage of bubble sort is that it can 
+detect whether the list is already sorted or not.
+*/
+
+
+void bubbleSort(vector <lld> &  num)
+{
+    lld temp=0;
+    int swapped;
+    FOR(i,0,(int)num.size()-1,1)
+    {
+        swapped=0;
+        FOR(j,0,(int)num.size()-2-i,1)
+        {
+
+             if(num[j]>num[j+1])
+             {
+                 temp=num[j];
+                 num[j]=num[j+1];
+                 num[j+1]=temp;
+                 swapped=1;
+
+             }
+
+
+
+        }
+
+          if(!swapped)
+             break;
+
+    }
+
+
+}
