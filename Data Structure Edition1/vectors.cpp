6a129524dc076038f0f04af8e65187f1347f7d18
rahul1809 <rahul.mishra2003@gmail.com>
2016-11-14 10:24:13 +0530

    iterators and vectors started

---
+++ Data Structure Edition1/vectors.cpp
@@ -0,0 +1,32 @@
+/*
+We will now start discussing about
+vectors , these are dynamically allocated arrays that is we change their sizes as per requirement
+,reserve space and avoid wasting memory uselessly
+
+
+*/
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+  // constructors used in the same order as described above:
+vector<int> first;                                // empty vector of ints
+vector<int> second (4,100);                       // four ints with value 100
+vector<int> third (second.begin(),second.end());  // iterating through second
+vector<int> fourth (third);                       // a copy of third
+
+  // the iterator constructor can also be used to construct from arrays:
+  int myints[] = {16,2,77,29};
+  vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+  cout << "The contents of fifth are:";
+  for (vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+    cout << ' ' << *it;
+  cout << '\n';
+
+  return 0;
+}
